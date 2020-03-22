dfd35f887b209c13b9e4cac12a7acff8c0b29c6c
kira-1072 <raufendro2@gmail.com>
2020-03-22 14:30:14 +0700

    tes

--- admin/admin.php
+++ admin/admin.php
@@ -50,7 +50,7 @@ print 'Page generated in : '. round(($finish - $start) * 10000, 2) .' <small>ms<
 	</a>
   		</div>
 		<div class="card-body">
-			<p class="card-text"><iframe src="http://192.168.0.102:5000/"></p>
+			<p class="card-text"><iframe width="660" height="500" src="http://192.168.0.100:5000/" frameborder="2" allow="autoplay" allowfullscreen></iframe><iframe width="660" height="500" src="http://192.168.0.100:5000/" frameborder="2" allow="autoplay" allowfullscreen></iframe><iframe width="660" height="500" src="http://192.168.0.100:5000/" frameborder="2" allow="autoplay" allowfullscreen></iframe></p>
 			
 		</div>
 
