ece5ecb91e93d8db94860405f1df0b629f779802
kira-1072 <raufendro2@gmail.com>
2020-03-22 18:22:29 +0700

    update

--- cek_login.php
+++ cek_login.php
@@ -3,7 +3,7 @@
 session_start();
 
 // menghubungkan dengan koneksi
-include 'koneksi.php';
+include '../config/koneksi.php';
 include 'ip2.php';
 // menangkap data yang dikirim dari form
 $username = $_POST['username'];
