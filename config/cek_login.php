1e551681c8a8d6d47e6a32bfa366360d17a799ec
kira-1072 <raufendro2@gmail.com>
2020-03-22 18:33:15 +0700

    New

--- config/cek_login.php
+++ config/cek_login.php
@@ -19,7 +19,7 @@ $cek = mysqli_num_rows($data);
 if($cek > 0){
 	$_SESSION['username'] = $username;
 	$_SESSION['status'] = "login";
-	header("Location: admin/admin.php");
+	header("Location: ../admin/admin.php");
 }else{
 	header("location:index.php?pesan=gagal");
 }
